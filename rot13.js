38a52823a8af60f8ad79ef67b91f7052692e23d0
Danielle Maier <danielle.e.maier@gmail.com>
2017-10-02 18:17:25 -0600

    second test passing ✔︎

--- rot13.js
+++ rot13.js
@@ -2,6 +2,8 @@ module.exports = {
   encode: function(str) {
     if (typeof str === 'undefined') {
       return undefined;
-    }
+    } else if (typeof str !== 'string') {
+      return "not a string";
+    } 
   }
 };
