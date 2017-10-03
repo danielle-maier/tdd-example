4a73ecc162198e84420aba1f54f7959deadcef11
Danielle Maier <danielle.e.maier@gmail.com>
2017-10-02 18:54:35 -0600

    3 tests passing ✰

--- rot13.js
+++ rot13.js
@@ -4,6 +4,17 @@ module.exports = {
       return undefined;
     } else if (typeof str !== 'string') {
       return "not a string";
+    } else {
+      let output = '';
+      for (let i = 0; i < str.length; i++) {
+        let c = str[i];
+        const code = str.charCodeAt(i);
+        if ((code >= 97) && (code <= 122)) {
+          c = String.fromCharCode(((code - 97 + 13) % 26) + 97);
+          output += c;
+        }
+      }
+      return output;
     }
   }
 };
