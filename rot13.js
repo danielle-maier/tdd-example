e2564f61227a624e30039e4230f431c89d5bddb9
Danielle Maier <danielle.e.maier@gmail.com>
2017-10-02 18:57:54 -0600

    works for upper and lower case now ✰

--- rot13.js
+++ rot13.js
@@ -9,10 +9,12 @@ module.exports = {
       for (let i = 0; i < str.length; i++) {
         let c = str[i];
         const code = str.charCodeAt(i);
-        if ((code >= 97) && (code <= 122)) {
+        if ((code >= 65) && (code <= 90)){
+		      c = String.fromCharCode(((code - 65 + 13) % 26) + 65);
+        } else if ((code >= 97) && (code <= 122)) {
           c = String.fromCharCode(((code - 97 + 13) % 26) + 97);
-          output += c;
         }
+        output += c;
       }
       return output;
     }
