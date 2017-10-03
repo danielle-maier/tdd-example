4f2b924700a3c54d846eecd30af63c5c2ad3c8c6
Danielle Maier <danielle.e.maier@gmail.com>
2017-10-02 18:05:39 -0600

    Revert "wrote tests ✘"
    This reverts commit e13a6187b28e89016bcc2c8993eb0933abba23fb.

--- rot13.js
+++ rot13.js
@@ -1,19 +1,5 @@
 module.exports = {
   encode: function(str) {
-    it('function should retun undefined if no string is passes', function() {
-      expect(rot13.encode()).to.equal(undefined);
-    });
-    it('function should return "not a string" if anything other than string is passed', function() {
-      expect(rot13.encode(13)).to.equal('not a string');
-    });
-    it('function should return "n" if "a" is entered', function() {
-      expect(rot13.encode('a')).to.equal('n');
-    });
-    it('function should return "a" if "n" is entered', function() {
-      expect(rot13.encode('n')).to.equal('a');
-    });
-    it('function should return encoded string', function() {
-      expect(rot13.encode('hello world')).to.equal('uryyb jbeyq')
-    });
+
   }
 };
