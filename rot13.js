2273b0049ff716cc43a4dbc829b9f1de09092127
Danielle Maier <danielle.e.maier@gmail.com>
2017-10-02 18:50:11 -0600

    fixing merge

--- rot13.js
+++ rot13.js
@@ -2,12 +2,6 @@ module.exports = {
   encode: function(str) {
     if (typeof str === 'undefined') {
       return undefined;
-<<<<<<< HEAD
     }
-=======
-    } else if (typeof str !== 'string') {
-      return "not a string";
-    } 
->>>>>>> 38a52823a8af60f8ad79ef67b91f7052692e23d0
   }
 };
