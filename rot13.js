c2328bf4d2cc03b115c8c62306ef66b5b8a1a96d
Danielle Maier <danielle.e.maier@gmail.com>
2017-10-06 12:13:33 -0600

    refactored for clarity

--- rot13.js
+++ rot13.js
@@ -1,11 +1,19 @@
 module.exports = {
   encode: function(str) {
+    let errorMsg = "not a string";
+    let upperCaseACode = 65;
+    let lowerCaseACode = 97;
+    let characterShift = 13;
+    let characterCount = 26;
     if (typeof str === 'undefined') {
       return undefined;
     } else if (typeof str !== 'string') {
-      return "not a string";
+      return errorMsg;
     } else {
-      return str.replace(/[a-z]/ig, c => Buffer([((d = Buffer(c)[0]) & 95) < 78 ? d + 13 : d - 13]));
+      return str.replace(/[a-zA-Z]/g, function(currentChar) {
+        var start = currentChar <= 'Z' ? upperCaseACode : lowerCaseACode;
+        return String.fromCharCode(start + (currentChar.charCodeAt(0) - start + characterShift) % characterCount); 
+      });
     }
   }
 };
